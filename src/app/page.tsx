d5e84ee8f7628c5d6dd20b0c373088d4410975a4
ahnji0520 <ahnji020520@gmail.com>
2025-05-17 22:25:05 +0900

    feat: change of file structure, bit of ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
-import Link from "next/link";
 
-export default function Home() {
+export default function LaunchPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/home");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main>
-      <h1>Sign in</h1>
-      <Link href="setup">Sign with Google</Link>
-      <br />
-      <Link href="evacuation">Login with Google</Link>
+    <main className="flex flex-col justify-between items-center h-screen bg-white">
+      <div className="flex-grow flex justify-center items-center">
+        <Image
+          src="/watch_out_logo.png"
+          alt="Watch Out Logo"
+          width={180}
+          height={180}
+        />
+      </div>
+
+      <div className="mb-8 text-center">
+        <h1 className="font-open font-bold text-[#5D5D5D] text-[36px]">
+          Watch Out!
+        </h1>
+        <p className="font-open font-normal text-[#5D5D5D] text-[16px] mb-[48px]">
+          Powered by Gemini AI
+        </p>
+      </div>
     </main>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        open: ['"Open Sans"', "sans-serif"],
+      },
+    },
+  },
+};
