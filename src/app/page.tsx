250a9252a5616a9f01df6a19a2cc4e1167a63251
Tetsuka <rogue1starwars728@yahoo.co.jp>
2025-05-17 20:16:44 +0900

    added setup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return <Link href="/evacuation">Evacuation Page </Link>;
+  return (
+    <main>
+      <h1>Sign in</h1>
+      <Link href="setup">Sign with Google</Link>
+      <br />
+      <Link href="evacuation">Login with Google</Link>
+    </main>
+  );
 }
---
+++ src/app/setup/page.tsx
@@ -0,0 +1,8 @@
+export default function Setup() {
+  return (
+    <main>
+      <h1>Setup</h1>
+      <p>Setup your device for the evacuation</p>
+    </main>
+  );
+}
